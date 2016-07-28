72050acd0ee8a556509789a89fb23199a5a59d72
Yadwinder Singh <ryker1990@gmail.com>
2016-07-29 03:04:10 +0530

    Update LIS3DHTR.c

--- C/LIS3DHTR.c
+++ C/LIS3DHTR.c
@@ -44,7 +44,7 @@ void main()
 	char data[1] = {0};
 	if(read(file, data, 1) != 1)
 	{
-		printf("Erorr : Input/output Erorr \n");
+		printf("Error : Input/Output error \n");
 		exit(1);
 	}
 	char data_0 = data[0];
